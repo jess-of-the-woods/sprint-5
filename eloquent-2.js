d973c8907240dc91bc998e93816b4e322d3cc880
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-02-14 17:26:32 +1300

    add work on chessboard, non-functioning

--- eloquent-2.js
+++ eloquent-2.js
@@ -20,3 +20,70 @@ do {
   console.log(result);
   result += "#"; 
 } while (result.length <= 8);
+
+
+
+
+//-----FizzBuzz------------
+
+
+for (n = 0; n <= 99; n++) {
+  if ((n % 3 === 0) && (n % 5 === 0)) {
+  console.log("FizzBuzz!");
+  }
+  else if (n % 3 === 0) {
+    console.log("Fizz");
+  }
+  else if (n % 5 === 0) {
+    console.log("Buzz");
+  }
+  else {
+    console.log(n);
+  }
+}
+
+
+///---------------------------------
+
+//--------ChessBoard
+
+var space = " ";
+var hash = "#";
+var chessString = "";
+var chessBoard = function(num) {
+  for (i = 0; i < num; i++) {
+    if ((i + c) % 2 === 0) {
+      chessString += space;
+    }
+    else {
+      chessString += hash;
+    };
+  for(c = 0; c < num; c++) {
+    chessString.length === num) {
+
+      }; // close inner loop (empty!)
+    chessString += "\n";
+  }; //close outer for loop
+  console.log(chessString);
+
+};
+
+chessBoard(8);
+
+
+
+/*
+
+__
+
+if (i + c) equals (num * 2) then break..
+
+
+    chessString += space;
+    chessString += hash; 
+    chessString += space;
+    chessString += hash; 
+    chessString += space;
+    chessString += hash; 
+    chessString += space;
+    chessString += hash; 
